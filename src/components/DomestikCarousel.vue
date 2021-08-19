8d9851d0043b1fbbc52c85a382900f440a9b0e3c
Diego Pertusa <mrjeffworker@MacBook-Pro-de-MRJEFF-6.local>
2021-08-19 18:05:04 +0200

    add basic project

---
+++ src/components/DomestikCarousel.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="domestik-carousel">
+    Slide 1
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DomestikCarousel'
+}
+</script>
+
+<style scoped>
+
+</style>
